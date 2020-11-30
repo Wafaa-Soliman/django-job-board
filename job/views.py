072304837db49766ff2bc75f7cfd2854d549b4ef
Wafaa Soliman <eng.wafaa.soliman@gmail.com>
2020-11-30 11:21:29 +0200

    add job app

--- job/views.py
+++ job/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Job
 
 # Create your views here.
 def job_list(request):
-    pass
+    job_list = Job.objects.all()
+    context = {'jobs':job_list}
+    return render(request, 'job/job_list.html', context)
 
 def job_detail(request, id):
-    pass
+    job_detail = get_object_or_404(Job, pk=id)
+    context={'job' : job_detail}
+    return render(request, 'job/job_detail.html', context)
